f0d5067f46199a3caf8c7418e1632ae6ff172b49
Canalstr <canalstrtrading@gmail.com>
2025-08-29 12:31:04 +0200

    Update footer.tsx
    Signed-off-by: Canalstr <canalstrtrading@gmail.com>

--- packages/email/components/footer.tsx
+++ packages/email/components/footer.tsx
@@ -7,11 +7,11 @@ export function Footer() {
 
       <Text className="font-regular text-[14px]">
         Open Source Compliance,{' '}
-        <Link href="https://trycomp.ai?utm_source=email&utm_medium=footer">Passt</Link>.
+        <Link href="https://passt.dev?utm_source=email&utm_medium=footer">Passt</Link>.
       </Text>
 
       <Text className="text-xs text-[#B8B8B8]">
-        Passt | 2261 Market Street, San Francisco, CA 94114
+        Passt | Keurenplein 4, 1069CD Amsterdam
       </Text>
     </Section>
   );
